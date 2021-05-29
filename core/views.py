b69f3d252e5b7a823cab601941e560deb38a9a7a
kim-hyunjin <hyunjin1612@gmail.com>
2021-05-29 17:07:55 +0900

    django rest framework

--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.http.response import HttpResponse
-from rooms.models import Room
-from django.core import serializers
-
-# Create your views here.
-def list_rooms(request):
-    rooms = serializers.serialize("json", Room.objects.all())
-    response = HttpResponse(content=rooms)
-    return response
