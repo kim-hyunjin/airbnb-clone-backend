1b5a00e1eb27f7dff64cf598c0fde12e82a1b378
kim-hyunjin <hyunjin1612@gmail.com>
2021-05-29 17:17:42 +0900

    ModelSerializer

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,7 +1,12 @@
+from rooms.models import Room
 from rest_framework import serializers
+from users.serializers import TinyUserSerializer
 
-class RoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length = 140)
-    price = serializers.IntegerField()
-    bedrooms = serializers.IntegerField()
-    instant_book = serializers.BooleanField()
+# ModelSerializer가 Meta에 명시한 모델을 보고, 명시한 필드를 serialize한다.
+class RoomSerializer(serializers.ModelSerializer):
+
+    user = TinyUserSerializer()
+    
+    class Meta:
+        model = Room
+        fields = ("name", "price", "instant_book", "user")
