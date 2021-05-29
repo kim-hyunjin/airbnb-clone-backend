b29de04931c3413c1ab46d303a5b87a81f33739e
Kim <hyunjin1612@gmail.com>
2021-05-29 19:45:16 +0900

    ModelViewSet

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from . import viewsets
+from rest_framework.routers import DefaultRouter
 
 app_name = "rooms"
 
-urlpatterns = [
-    path("list/", views.ListRoomsView.as_view()),
-    path("<int:pk>/", views.SeeRoomView.as_view()),
-]
+router = DefaultRouter()
+router.register("", viewsets.RoomViewSet, basename='room')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path("list/", views.ListRoomsView.as_view()),
+#     path("<int:pk>/", views.SeeRoomView.as_view()),
+# ]
