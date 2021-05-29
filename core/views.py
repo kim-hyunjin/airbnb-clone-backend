49a12e1987e47f7a13aaa717b548a2054f8b58c9
kim-hyunjin <hyunjin1612@gmail.com>
2021-05-29 16:48:14 +0900

    http통신 예시

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from rooms.models import Room
+from django.core import serializers
 
 # Create your views here.
+def list_rooms(request):
+    rooms = serializers.serialize("json", Room.objects.all())
+    response = HttpResponse(content=rooms)
+    return response
