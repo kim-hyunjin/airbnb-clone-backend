3d3ed0af04e1331b0f32b18a885d0194d8c11dd3
kim-hyunjin <hyunjin1612@gmail.com>
2021-05-29 17:24:29 +0900

    rest_framework 클래스뷰

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,17 @@
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer
 
-@api_view(["GET"])
-def list_rooms(request):
-    rooms = Room.objects.all()
-    serialized_rooms = RoomSerializer(rooms, many=True)
-    return Response(data=serialized_rooms.data)
+class ListRoomsView(APIView):
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(data=serializer.data)
+
+# @api_view(["GET"])
+# def list_rooms(request):
+#     rooms = Room.objects.all()
+#     serialized_rooms = RoomSerializer(rooms, many=True)
+#     return Response(data=serialized_rooms.data)
