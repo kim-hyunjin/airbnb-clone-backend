8691b9470a195b1068af57cca052e09c7c775bd3
Kim <hyunjin1612@gmail.com>
2021-05-29 19:18:41 +0900

    ListAPIView, pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,19 @@
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Room
 from .serializers import RoomSerializer
 
-class ListRoomsView(APIView):
-    def get(self, request):
-        rooms = Room.objects.all()
-        serializer = RoomSerializer(rooms, many=True)
-        return Response(data=serializer.data)
+class ListRoomsView(ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+# class ListRoomsView(APIView):
+#     def get(self, request):
+#         rooms = Room.objects.all()
+#         serializer = RoomSerializer(rooms, many=True)
+#         return Response(data=serializer.data)
 
 # @api_view(["GET"])
 # def list_rooms(request):
