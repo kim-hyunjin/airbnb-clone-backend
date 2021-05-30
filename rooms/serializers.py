22611a839ded667acc25a43553aa22b64895f123
Kim <hyunjin1612@gmail.com>
2021-05-30 21:18:08 +0900

    UserSerializer 하나로 합치기, User Create완료

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,11 +1,11 @@
 from rooms.models import Room
 from rest_framework import serializers
-from users.serializers import RelatedUserSerializer
+from users.serializers import UserSerializer
 
 # ModelSerializer가 Meta에 명시한 모델을 보고, 명시한 필드를 serialize한다.
 class RoomSerializer(serializers.ModelSerializer):
 
-    user = RelatedUserSerializer()
+    user = UserSerializer()
     
     class Meta:
         model = Room
--- users/views.py
+++ users/views.py
@@ -1,36 +1,38 @@
 from rooms.models import Room
 from rooms.serializers import RoomSerializer
 from users.models import User
-from users.serializers import ReadUserSerializer, WriteUserSerializer
+from users.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+class UsersView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        new_user = serializer.save()
+        return Response(UserSerializer(new_user).data)
+
+
 class MeView(APIView):
 
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response(ReadUserSerializer(request.user).data)
+        return Response(UserSerializer(request.user).data)
 
     def put(self, request):
-        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
         return Response()
 
-
-@api_view(["GET"])
-def user_detail(request, pk):
-    try:
-        user = User.objects.get(pk=pk)
-        return Response(ReadUserSerializer(user).data)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
 class FavsView(APIView):
 
@@ -57,3 +59,11 @@ class FavsView(APIView):
         except Room.DoesNotExist:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(["GET"])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        return Response(UserSerializer(user).data)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
