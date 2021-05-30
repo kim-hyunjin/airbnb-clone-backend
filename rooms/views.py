f61f25db003eab87b744b1b1983f133d9e89702f
Kim <hyunjin1612@gmail.com>
2021-05-30 19:11:42 +0900

    room delete

--- rooms/views.py
+++ rooms/views.py
@@ -6,16 +6,6 @@ from rest_framework import status
 from .models import Room
 from .serializers import RoomSerializer, WriteRoomSerializer
 
-# class ListRoomsView(ListAPIView):
-#     queryset = Room.objects.all()
-#     serializer_class = RoomSerializer
-
-# class ListRoomsView(APIView):
-#     def get(self, request):
-#         rooms = Room.objects.all()
-#         serializer = RoomSerializer(rooms, many=True)
-#         return Response(data=serializer.data)
-
 # @api_view(["GET", "POST"])
 # def list_rooms(request):
 #     if request.method == "GET":
@@ -51,20 +41,34 @@ class RoomsView(APIView):
             room = serializer.save(user=request.user)
             return Response(data=RoomSerializer(room).data, status=status.HTTP_200_OK)
         else:
-            return Response(dara=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# url로 넘어오는 pk를 사용해 알아서 Room을 찾아준다.
 class RoomView(APIView):
 
-    def get(self, request, pk):
+    def get_room(self, pk):
         try:
             room = Room.objects.get(pk=pk)
-            return Response(RoomSerializer(room).data)
+            return room
         except Room.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
+            serializer = RoomSerializer(room).data
+            return Response(serializer)
+        else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
     def put(self, request):
         pass
 
-    def delete(self, request):
-        pass
+    def delete(self, request, pk):
+        room = self.get_room(pk)
+        if room.user != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        if room is not None:
+            room.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
