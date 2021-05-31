30d071d72b7366d389995d630a9dae2b854ecd47
kim-hyunjin <hyunjin1612@gmail.com>
2021-05-31 21:39:59 +0900

    jwt

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+from jwt import encode
+from django.contrib.auth import authenticate
+from django.conf import settings
 from rooms.models import Room
 from rooms.serializers import RoomSerializer
 from users.models import User
@@ -67,3 +70,17 @@ def user_detail(request, pk):
         return Response(UserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    user = authenticate(username=username, password=password)
+    if user is None:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    encoded_jwt = encode({"id": user.pk}, settings.SECRET_KEY, algorithm="HS256")
+    return Response(data={"token": encoded_jwt})
