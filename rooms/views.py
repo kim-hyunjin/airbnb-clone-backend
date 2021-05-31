78dfde2c87ebd8fbd0112c68ee25e0db1d68c683
kim-hyunjin <hyunjin1612@gmail.com>
2021-05-31 22:33:13 +0900

    room search - 필터링

--- rooms/views.py
+++ rooms/views.py
@@ -94,8 +94,31 @@ class RoomView(APIView):
 
 @api_view(["GET"])
 def room_search(request):
+    max_price = request.GET.get("max_price", None)
+    min_price = request.GET.get("min_price", None)
+    beds = request.GET.get("beds", None)
+    bedrooms = request.GET.get("bedrooms", None)
+    bathrooms = request.GET.get("bathrooms", None)
+
+    # Django queryset filter 만들기
+    filter_kwargs = {}
+    if max_price is not None:
+        filter_kwargs["price__lte"] = max_price
+    if min_price is not None:
+        filter_kwargs["price__gte"] = min_price
+    if beds is not None:
+        filter_kwargs["beds__gte"] = beds
+    if bedrooms is not None:
+        filter_kwargs["bedrooms__gte"] = bedrooms
+    if bathrooms is not None:
+        filter_kwargs["bathrooms__gte"] = bathrooms
+
     paginator = OwnPagination()
-    rooms = Room.objects.filter()
+    try:
+        rooms = Room.objects.filter(**filter_kwargs) # **를 사용하면 dictionary가 price__lte='30', beds__gte='2', bathrooms__gte='2'와 같이 된다.
+    except ValueError:
+        rooms = Room.objects.all()
+        
     results = paginator.paginate_queryset(rooms, request)
     serialized_room = RoomSerializer(results, many=True).data
     return paginator.get_paginated_response(serialized_room)
