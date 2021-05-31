c59c6e796bdcf3e5a69b47872891db46ac2f0966
kim-hyunjin <hyunjin1612@gmail.com>
2021-05-31 22:41:09 +0900

    room filter 추가, 위도경도로 방찾기

--- rooms/views.py
+++ rooms/views.py
@@ -99,6 +99,8 @@ def room_search(request):
     beds = request.GET.get("beds", None)
     bedrooms = request.GET.get("bedrooms", None)
     bathrooms = request.GET.get("bathrooms", None)
+    lat = request.GET.get("lat", None)
+    lng = request.GET.get("lng", None)
 
     # Django queryset filter 만들기
     filter_kwargs = {}
@@ -112,7 +114,12 @@ def room_search(request):
         filter_kwargs["bedrooms__gte"] = bedrooms
     if bathrooms is not None:
         filter_kwargs["bathrooms__gte"] = bathrooms
-
+    if lat is not None and lng is not None:
+        filter_kwargs["lat__gte"] = float(lat) - 0.005
+        filter_kwargs["lat__lte"] = float(lat) + 0.005
+        filter_kwargs["lng__gte"] = float(lng) - 0.005
+        filter_kwargs["lng__lte"] = float(lng) + 0.005
+    
     paginator = OwnPagination()
     try:
         rooms = Room.objects.filter(**filter_kwargs) # **를 사용하면 dictionary가 price__lte='30', beds__gte='2', bathrooms__gte='2'와 같이 된다.
