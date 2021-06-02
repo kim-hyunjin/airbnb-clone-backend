72ea284da06e63273d336ad613bd2a4605ad53b1
kim-hyunjin <hyunjin1612@gmail.com>
2021-06-02 21:55:10 +0900

    use graphene_django get rooms

--- config/schema.py
+++ config/schema.py
@@ -1,10 +1,20 @@
 import graphene
+from rooms.models import Room
+from graphene_django import DjangoObjectType
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    rooms = graphene.List(RoomType)
 
     def resolve_hello(self, info):
         return "Hello"
+
+    def resolve_rooms(self, info):
+        return Room.objects.all()
 
 class Mutation():
     pass
