2494dad014eeb58eb564b690fcdc39d5ec707b69
kim-hyunjin <hyunjin1612@gmail.com>
2021-06-02 21:40:26 +0900

    graphql hello

---
+++ config/schema.py
@@ -0,0 +1,12 @@
+import graphene
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(self, info):
+        return "Hello"
+
+class Mutation():
+    pass
+
+schema = graphene.Schema(query=Query)
