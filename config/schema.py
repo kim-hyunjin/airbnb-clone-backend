5d5cd2a1c6dd97d6deab2624fedb407db96433d0
kim-hyunjin <hyunjin1612@gmail.com>
2021-06-03 19:56:34 +0900

    schema 분리

--- config/schema.py
+++ config/schema.py
@@ -1,20 +1,24 @@
 import graphene
-from rooms.models import Room
-from graphene_django import DjangoObjectType
+# from rooms.models import Room
+# from graphene_django import DjangoObjectType
+from rooms import schema as rooms_schema
 
-class RoomType(DjangoObjectType):
-    class Meta:
-        model = Room
 
-class Query(graphene.ObjectType):
-    hello = graphene.String()
-    rooms = graphene.List(RoomType)
+# class RoomType(DjangoObjectType):
+#     class Meta:
+#         model = Room
 
-    def resolve_hello(self, info):
-        return "Hello"
+# class Query(graphene.ObjectType):
+#     hello = graphene.String()
+#     rooms = graphene.List(RoomType)
 
-    def resolve_rooms(self, info):
-        return Room.objects.all()
+#     def resolve_hello(self, info):
+#         return "Hello"
+
+#     def resolve_rooms(self, info):
+#         return Room.objects.all()
+class Query(rooms_schema.Query, graphene.ObjectType):
+    pass
 
 class Mutation():
     pass
---
+++ users/schema.py
@@ -0,0 +1,9 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import User
+
+class UserType(DjangoObjectType):
+    rooms = graphene.List("rooms.schema.RoomType")
+
+    class Meta:
+        model = User
