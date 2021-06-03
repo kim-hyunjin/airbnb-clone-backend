464a7d96d9bcbb04d8b6e4089f0047b3d050446a
kim-hyunjin <hyunjin1612@gmail.com>
2021-06-03 20:15:11 +0900

    페이지네이션

--- rooms/schema.py
+++ rooms/schema.py
@@ -8,8 +8,18 @@ class RoomType(DjangoObjectType):
     class Meta:
         model = Room
 
-class Query(object):
-    rooms = graphene.List(RoomType)
+class RoomListResponse(graphene.ObjectType):
 
-    def resolve_rooms(self, info):
-        return Room.objects.all()
+    arr = graphene.List(RoomType)
+    total = graphene.Int()
+
+class Query(object):
+    rooms = graphene.Field(RoomListResponse, page=graphene.Int())
+
+    def resolve_rooms(self, info, page=1):
+        page_size = 5
+        skipping = page_size * (page - 1)
+        taking = page_size * page
+        rooms = Room.objects.all()[skipping:taking]
+        total = Room.objects.count()
+        return RoomListResponse(arr=rooms, total=total)
