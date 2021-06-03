a561ad5f295518cf9fe9479bd91e74705924cd0a
kim-hyunjin <hyunjin1612@gmail.com>
2021-06-03 23:20:07 +0900

    toggle favs

--- users/schema.py
+++ users/schema.py
@@ -1,14 +1,23 @@
 import graphene
 from .types import UserType
 from .models import User
-from .mutations import CreateAccountMutation, LoginMutation
+from .mutations import CreateAccountMutation, LoginMutation, ToggleFavsMutation
 
 class Query(object):
     user = graphene.Field(UserType, id=graphene.Int(required=True))
+    me = graphene.Field(UserType)
 
     def resolve_user(self, info, id):
         return User.objects.get(id=id)
 
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_authenticated:
+            return info.context.user
+        else:
+            raise Exception("You need to be logged in")
+
 class Mutation(object):
     create_account = CreateAccountMutation.Field()
     login = LoginMutation.Field()
+    toggle_favs = ToggleFavsMutation.Field()
